5d3a403bae8855fe9960bcf6599ae7a78c67f719
Hcki186 <hackicrew186@gmail.com>
2024-03-05 11:50:58 +0100

    adding views file and path for file

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,36 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: "/",
+      name: "HomePage",
+      component: () =>
+        import("../views/HomePage.vue")
+    },
+    {
+      path: "/Informácie ",
+      name: "InfoPage",
+      component: () =>
+        import("../views/InfoPage.vue")
+    },
+    {
+      path: "/Dokumenti",
+      name: "DokumentPage",
+      component: () =>
+        import("../views/DocumentPage.vue")
+    },
+    {
+      path: "/Galéria",
+      name: "GalleryPage",
+      component: () =>
+        import("../views/GalleryPage.vue")
+    },
+    {
+      path: "/Kontakt",
+      name: "ContactPage",
+      component: () =>
+        import("../views/ContactPage.vue")
     }
   ]
 })
