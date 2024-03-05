7a22f8ecbe335ba3431b9bd67febb5cf07a99b3b
Hcki186 <hackicrew186@gmail.com>
2024-03-05 19:17:18 +0100

    Nav Bar demo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import NavBar from "./components/NavBar.vue"
 </script>
 
 <template>
+  <NavBar></NavBar>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
---
+++ src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-md fixed-top bg-body-tertiary">
+            <div class="container-md">
+                <div class="navbar-brand ">
+                    <img src="../assets/img/MalySvetLogo/Logo.png" alt="MalySvet" width="40" height="40">
+                </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/">Domov</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/Informácie">Informácie</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/Dokumenti">Dokumenti</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/Galéria">Galéria</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/Kontakt">Kontakt</router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
