e233af6e22f24a720d623136cf967301c5a71c18
Hcki186 <hackicrew186@gmail.com>
2024-09-11 21:12:28 +0200

    contact Page

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="TopGap">
         <GoogleMapLoc></GoogleMapLoc>
+        <AboutAs></AboutAs>
     </div>
 </template>
   
   <script setup lang="ts">
     import GoogleMapLoc from '../components/ContactPage/GoogleMapLoc.vue';
+    import AboutAs from "../components/ContactPage/MeInformation.vue";
   </script>
   
