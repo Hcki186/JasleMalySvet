60ab4349e9edd57015ff46089e48c629e569eab7
Hcki186 <hackicrew186@gmail.com>
2024-09-11 21:12:08 +0200

    Info page

--- src/components/InfoPage/ListOfServices.vue
+++ src/components/InfoPage/ListOfServices.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="ListOfServices">
         <div class="container-md Modul">
-            <h1>Služby ktore ponukame</h1> 
-            <div class="list-group">
-                <a class="list-group-item list-group-item-action " >Celodenná starostlivosť na mesiac(so stravou)</a>
-                <a class="list-group-item list-group-item-action">poldenná starostlivosť od 7:00 hod. - do 12:00 hod. (so stravou)</a>
-                <a class="list-group-item list-group-item-action">Celodenná starostlivosť na deň (so stravou)</a>
-                <a class="list-group-item list-group-item-action">Hodinova starostlivosť ( časove rozmedzie od 7:00 hod. - do 16:30 hod.)</a>
+            <div class="borrder">
+                <h1>Služby ktore ponukame</h1> 
+                <ul class="list-group">
+                    <li>Celodenná starostlivosť na mesiac(so stravou)</li>
+                    <li>poldenná starostlivosť od 7:00 hod. - do 12:00 hod. (so stravou)</li>
+                    <li>Celodenná starostlivosť na deň (so stravou)</li>
+                    <li>Hodinova starostlivosť ( časove rozmedzie od 7:00 hod. - do 16:30 hod.)</li>
+                </ul>
             </div>
         </div>
     </div>
--- src/components/InfoPage/ListOfThings.vue
+++ src/components/InfoPage/ListOfThings.vue
@@ -1,25 +1,26 @@
 <template>
     <div class="ListOfThings">
-        
         <div class="container-md Modul">
-            <h1>V jaslach poskytujeme:</h1>
-            <ul>
-                <li>Celodenu stravu, pitni režim a ovocie</li>
-                <li>postelnu bielizeň</li>
-                <li>podbradniky</li>
-                <li>Papierove hygienecke vreckovky a tvaletni papier</li>
-                <li>Utierak</li>
-            </ul>
-            <h1>TEXT TEXT:</h1>
-            <ul>
-                <li>Prezuvky (alebo hrubšie ponožky)</li>
-                <li> Pyžamo</li>
-                <li>Veci na prezlečenie (tepláky, tríčko, spodnú bielizeň)</li>
-                <li>Fľašu na pitie</li>
-                <li>Obľubenu hračku na spanie (ak má aj cumlík)</li>
-                <li>Dieťa nosiace plienky: plienky, navlhčené vreckovky, krém proti zapareniu- napr. SUDOCREM</li>
-            </ul>
-            <h2>Všetky veci treba označiť menom Ďakujem :D</h2>
+            <div class="borrder">
+                <h1>V jaslach poskytujeme:</h1>
+                <ul>
+                    <li>Celodenu stravu, pitni režim a ovocie</li>
+                    <li>postelnu bielizeň</li>
+                    <li>podbradniky</li>
+                    <li>Papierove hygienecke vreckovky a tvaletni papier</li>
+                    <li>Utierak</li>
+                </ul>
+                <h1>Veci ktoré treba doniesť:</h1>
+                <ul>
+                    <li>Prezuvky (alebo hrubšie ponožky)</li>
+                    <li> Pyžamo</li>
+                    <li>Veci na prezlečenie (tepláky, tríčko, spodnú bielizeň)</li>
+                    <li>Fľašu na pitie</li>
+                    <li>Obľubenu hračku na spanie (ak má aj cumlík)</li>
+                    <li>Dieťa nosiace plienky: plienky, navlhčené vreckovky, krém proti zapareniu- napr. SUDOCREM</li>
+                </ul>
+                <h2>Všetky veci treba označiť menom Ďakujem :D</h2>
+            </div>
         </div>
     </div>
 </template>
