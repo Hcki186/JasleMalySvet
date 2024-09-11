bcc4da3854562e89f9ee971bb6cd4227fe1a3299
Hcki186 <hackicrew186@gmail.com>
2024-09-11 21:18:25 +0200

    adding footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <NavBar></NavBar>
   <RouterView />
+  <FooterBar></FooterBar>
 </template>
 
 
 
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterView } from 'vue-router'
 import NavBar from "./components/NavBar.vue"
+import FooterBar from "./components/FooterBar.vue"
 </script>
-
 
 <style >
 </style>
